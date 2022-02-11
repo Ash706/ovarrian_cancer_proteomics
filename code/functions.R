d4b368f56f6b5b79cb441dc736fa720a13aec263
Ash706 <ashfaq.ali@nbis.se>
2022-02-11 08:23:43 +0100

    update 2

---
+++ code/functions.R
@@ -0,0 +1,41 @@
+
+##
+###' Convert tibbles to a matrix
+#'
+#' Mainly used to convert count tables to matrix for Bioconductor functions that
+#' require a matrix as input.
+#'
+#' @param x a tibble with identifiers in column 1 (assigned as rownames) and
+#'  numbers in remaining columns
+#' @return A matrix
+#' @author Chris Stubben
+#'
+#' @examples
+#' x < top_counts(pasilla$results, pasilla$rlog, top=25)
+#' x
+#' as_matrix(x)
+#' @export
+
+
+as_matrix <- function(x){
+    if(!tibble::is_tibble(x) ) stop("x must be a tibble")
+    y <- as.matrix.data.frame(x[,-1])
+    rownames(y) <- x[[1]]
+    y
+}
+
+#' @describeIn as_matrix Convert matrix to tibble
+#' @param var name of column, default id
+#' @export
+
+as_tbl <- function(x, var="id"){
+    if(!is.matrix(x)) stop("x must be a matrix")
+    tibble::as_tibble(tibble::rownames_to_column(data.frame(x, check.names=FALSE), var))
+}
+
+
+## 
+###' Apply loess and vsn normalization to a SummarizedExperiment(Se) and return and object of the Se class
+###' @param x an object of the the class SummarizedExperimeny
+###' @param assay name of the assay to be normalized (e.g., raw data)
+###' @param method 
